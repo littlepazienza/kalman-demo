1f1561eeb9047948e2a7ceefcb94ed206895c9e3
bpazienza <ben.pazienza@alertinnovation.com>
2024-08-08 13:45:20 -0400

    Adding configurable error functions for rotation and movement

--- www/index.js
+++ www/index.js
@@ -1,11 +1,12 @@
 import { Universe } from "@littlepaz/kalman-demo";
 
 const GRID_COLOR = "#CCCCCC";
-const KALMAN_COLOR = "#850c5d";
+const GOAL_COLOR = "#e60606";
+const KALMAN_COLOR = "#000000";
 
 // Construct the universe, and get its width and height.
 const CELL_SIZE = 10;
-const universe = Universe.new(30.0, 30.0);
+const universe = Universe.new(1.0, 1.0);
 const width = universe.width() / 10;
 const height = universe.height() / 10;
 
@@ -17,6 +18,8 @@ canvas.width = universe.height() + CELL_SIZE;
 
 const ctx = canvas.getContext('2d');
 
+let x = 0
+
 const renderLoop = async () => {
     universe.tick();
 
@@ -24,6 +27,28 @@ const renderLoop = async () => {
 
     drawGrid();
     drawUniverse();
+
+    if (universe.kalman().get_velocity() > 0) {
+        x += 1
+        const actual = universe.kalman().get_actual()
+        const belief = universe.kalman().get_belief()
+        const actual_txt = document.getElementById('actual')
+        const belief_txt = document.getElementById('belief')
+        actual_txt.value = actual_txt.value + `${x} > [x: ${actual[0].toFixed(2)}, y: ${actual[1].toFixed(2)}, v: ${actual[2].toFixed(2)}, θ: ${actual[3].toFixed(2)}]\n`
+        belief_txt.value = belief_txt.value + `${x} > [x: ${belief[0].toFixed(2)}, y: ${belief[1].toFixed(2)}, v: ${belief[2].toFixed(2)}, θ: ${belief[3].toFixed(2)}]\n`
+
+        // Make sure the rolling text areas keep their scroll on the bottom by default
+        actual_txt.scrollTop = actual_txt.scrollHeight;
+        belief_txt.scrollTop = belief_txt.scrollHeight;
+    }
+
+    // Paint the error debug info
+    const rotation_error = universe.kalman().get_rotation_error()
+    const movement_error = universe.kalman().get_movement_error()
+    const rotation_txt = document.getElementById('rotation_error')
+    const movement_txt = document.getElementById('movement_error')
+    rotation_txt.value = `N(${rotation_error[0].toFixed(5)}, ${rotation_error[1].toFixed(5)})`
+    movement_txt.value = `N(${movement_error[0].toFixed(5)}, ${movement_error[1].toFixed(5)})`
 
     // The tick rate is 1ms
     await delay(1);
@@ -58,28 +83,54 @@ const drawGrid = () => {
 
 const drawUniverse = () => {
     ctx.beginPath();
+    ctx.fillStyle = GOAL_COLOR;
+    const goal = universe.kalman().get_goal()
+    if (goal[0] > 0 && goal[1] > 0) {
+        ctx.fillRect(
+            goal[0],
+            goal[1],
+            CELL_SIZE,
+            CELL_SIZE
+        )
+    }
+    ctx.stroke();
 
-    ctx.strokeStyle = KALMAN_COLOR;
+    ctx.beginPath();
+    ctx.fillStyle = KALMAN_COLOR;
     ctx.fillRect(
         universe.kalman().get_x(),
         universe.kalman().get_y(),
         CELL_SIZE,
         CELL_SIZE
     );
-
     ctx.stroke();
 };
 
-drawGrid();
-drawUniverse();
-renderLoop();
 
 document.getElementById('set_goal').addEventListener('click', function(e)
 {
     setGoal();
 }, false);
+document.getElementById('set_rotation_error').addEventListener('click', function(e)
+{
+    setRotationError();
+}, false);
+document.getElementById('set_movement_error').addEventListener('click', function(e)
+{
+    setMovementError();
+}, false);
 
+
+drawGrid();
+drawUniverse();
+renderLoop();
 const setGoal = () => {
     universe.set_kalman_goal(document.getElementById("x").value, document.getElementById("y").value)
+}
+const setMovementError = () => {
+    universe.set_kalman_movement_error(document.getElementById("movement_m").value, document.getElementById("movement_s").value)
+}
+const setRotationError = () => {
+    universe.set_kalman_rotation_error(document.getElementById("rotation_m").value, document.getElementById("rotation_s").value)
 }
 
