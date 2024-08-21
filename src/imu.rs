f1c438f270c55210f2ab2aec5ad04fd6aa56e631
bpazienza <ben.pazienza@alertinnovation.com>
2024-08-21 10:42:13 -0400

    It compiles

--- src/imu.rs
+++ src/imu.rs
@@ -1,7 +1,8 @@
-use ndarray::{arr2, Array2};
-use rstat::Distribution;
-use rstat::linalg::Matrix;
-use rstat::normal::{MvNormal, MvNormalParams};
+use std::ops::Mul;
+use statrs::distribution::{MultivariateNormal};
+use nalgebra::{DVector, DMatrix, VecStorage, Matrix, Dyn};
+use rand::distributions::Distribution;
+use statrs::statistics::{MeanN, VarianceN};
 use wasm_bindgen::prelude::wasm_bindgen;
 use crate::{log, Universe};
 
@@ -11,57 +12,62 @@ pub struct Imu {
     /*
      * The emulated real position of kalman.
      */
-    actual_pos: Vec<f32>,
+    actual_pos: Vec<f64>,
 
     /*
      * The bivariate distribution of error for reading the rotational angle and velocity of kalman.
      */
-    distribution: Box<MvNormal>,
+    distribution: Box<MultivariateNormal>,
 }
 
 impl Imu {
-    pub fn new(x: f32, y: f32) -> Imu {
-        let cov = arr2(&[
-            [0.001, 0.1],
-            [0.1, 0.001]
-        ]);
+    pub fn new(x: f64, y: f64) -> Imu {
         Imu {
             actual_pos: Vec::from([x, y, 0.0, 0.0]),
-            distribution: Box::new(MvNormal::new(vec![0.0, 0.0], cov).unwrap()),
+            distribution: Box::new(MultivariateNormal::new(vec![0., 0.], vec![0.001, 0.1, 0.1, 0.001]).unwrap()),
         }
     }
 
-    pub fn get_actual_pos(&self) -> Vec<f32> {
+    pub fn get_actual_pos(&self) -> Vec<f64> {
         return self.actual_pos.clone();
     }
 
-    pub unsafe fn get_error(&self) -> MvNormalParams {
-        log(&format!("Movement error is: N({:?}, {:?})", (*self.distribution).params().mu, (*self.distribution).params().Sigma));
-        (*self.distribution).params()
+    pub unsafe fn get_error_mean(&self) -> DVector<f64> {
+        log(&format!("Movement error mean is:{:?})", (*self.distribution).mean()));
+        (*self.distribution).mean().unwrap()
     }
 
-    pub fn set_error(&mut self, m: Vec<f32>, covariance_matrix: Matrix<f32>) {
+    pub unsafe fn get_error_cov(&self) -> DMatrix<f64> {
+        log(&format!("Movement error covariance is:{:?})", (*self.distribution).variance()));
+        (*self.distribution).variance().unwrap()
+    }
+
+    pub fn set_error(&mut self, m: Vec<f64>, covariance_matrix: Vec<f64>) {
         log(&format!("Setting my rotation error to N({:?}, {:?})", m, covariance_matrix));
-        *self.distribution = MvNormal::new(m, covariance_matrix).unwrap()
+        *self.distribution = MultivariateNormal::new(m, covariance_matrix).unwrap()
     }
 
-    pub unsafe fn simulate(&mut self, velocity: f32, theta: f32, universe: Universe) {
+    pub unsafe fn simulate(&mut self, velocity: f64, theta: f64, universe: Universe) {
         log(&format!("Simulating 1 ms with rotation {}, velocity {}, position {}, {}", theta, velocity, self.actual_pos[0], self.actual_pos[1]));
 
+        let sample = (*self.distribution).sample(&mut rand::thread_rng());
+        let vel_error = sample[0];
+        let rot_error = sample[1];
+
         // Update the velocity
-        self.actual_pos[2] = self.generate_velocity_with_error(velocity);
+        self.actual_pos[2] = velocity + velocity * vel_error;
         log(&format!("Commanded velocity: {}, actual: {}", velocity, self.actual_pos[2]));
 
         // Update the rotation
-        self.actual_pos[3] = self.generate_rotation_with_error(theta);
+        self.actual_pos[3] = theta + theta * rot_error;
         log(&format!("Commanded rotation: {}, actual: {}", theta, self.actual_pos[3]));
 
         // If movement is expected, introduce movement variability from unknown variables
-        if velocity != 0f32 {
+        if velocity != 0. {
             // Update the x position
-            self.actual_pos[0] = (self.actual_pos[0] + self.actual_pos[3].cos() * self.actual_pos[2]).max(0f32).min(universe.width as f32);
+            self.actual_pos[0] = (self.actual_pos[0] + self.actual_pos[3].cos() * self.actual_pos[2]).max(0.).min(universe.width as f64);
             // Update the y position
-            self.actual_pos[1] = (self.actual_pos[1] + self.actual_pos[3].sin() * self.actual_pos[2]).max(0f32).min(universe.height as f32);
+            self.actual_pos[1] = (self.actual_pos[1] + self.actual_pos[3].sin() * self.actual_pos[2]).max(0.).min(universe.height as f64);
         }
     }
 
@@ -69,15 +75,16 @@ impl Imu {
      * Read the actual velocity achieved by kalman, plus some error.
      */
     pub unsafe fn read_imu(&self) -> ImuRead {
-        let sample = (*self.distribution).sample();
-        let velocity = sample[0];
-        let rotation = sample[1];
+        let sample = (*self.distribution).sample(&mut rand::thread_rng());
+        let vel_error = sample[0];
+        let rot_error = sample[1];
+        let velocity = self.actual_pos[2] + self.actual_pos[2] * vel_error;
+        let rotation = self.actual_pos[3] + self.actual_pos[3] * rot_error;
+        let matrix: DMatrix<f64> = DMatrix::from_vec(2, 2, vec![velocity, 1.0, 1.0, rotation]);
+        let matrix1: Matrix<f64, Dyn, Dyn, VecStorage<f64, Dyn, Dyn>> = (*self.distribution).variance().unwrap();
         ImuRead {
-            state: Vec::from([velocity, rotation]),
-            covariance_matrix: arr2(&[
-                [velocity, 1.0],
-                [1.0, rotation]
-            ]) * self.distribution.params().Sigma
+            state: DVector::from_vec(vec![velocity, rotation]),
+            covariance_matrix: matrix * matrix1
         }
     }
  }
@@ -85,16 +92,16 @@ impl Imu {
 #[wasm_bindgen]
 #[derive(Clone)]
 pub struct ImuRead {
-    state: Vec<f32>,
-    covariance_matrix: Array2<f32>
+    state: DVector<f64>,
+    covariance_matrix: DMatrix<f64>
 }
 
 impl ImuRead {
-    pub fn get_state(&self) -> Vec<f32> {
+    pub fn get_state(&self) -> DVector<f64> {
         self.state.clone()
     }
 
-    pub fn get_covariance_matrix(&self) -> Array2<f32> {
+    pub fn get_covariance_matrix(&self) -> DMatrix<f64> {
         self.covariance_matrix.clone()
     }
 }
--- src/kalman.rs
+++ src/kalman.rs
@@ -1,12 +1,10 @@
-use std::ops::{Add, Index, Mul, Sub};
+use std::ops::{Add, Mul, Sub};
 use std::vec::Vec;
+use nalgebra::{DMatrix, DVector};
 use wasm_bindgen::prelude::wasm_bindgen;
 use crate::{log, Universe, imu::Imu};
-use ndarray::{arr2, Array2};
-use rstat::linalg::Matrix;
-use rstat::normal::MvNormalParams;
 
-static VELOCITY: f32 = 1f32;
+static VELOCITY: f64 = 1.;
 
 #[wasm_bindgen]
 #[derive(Clone)]
@@ -14,22 +12,22 @@ pub struct Kalman {
     /*
      * The believed position of kalman.
      */
-    belief_pos: Vec<f32>,
+    belief_pos: Vec<f64>,
 
     /*
      * The believed state of kalman.
      */
-    state: Vec<f32>,
+    state: Vec<f64>,
 
     /*
      * The accrued covariance matrix
      */
-    covariance_matrix: Box<Array2<f32>>,
+    covariance_matrix: Box<DMatrix<f64>>,
 
     /*
      * The goal x and y.
      */
-    goal: Vec<f32>,
+    goal: Vec<f64>,
 
     /*
      * The emulated inertial measurement unit (IMU).
@@ -39,39 +37,42 @@ pub struct Kalman {
 
 #[wasm_bindgen]
 impl Kalman {
-    pub fn get_goal(&self) -> Vec<f32> {
-        return self.goal.clone()
+    pub fn get_goal(&self) -> Vec<f64> {
+        self.goal.clone()
     }
 
-    pub fn get_belief(&self) -> Vec<f32> {
+    pub fn get_belief(&self) -> Vec<f64> {
         Vec::from([self.belief_pos[0], self.belief_pos[1], self.state[0], self.state[1]])
     }
 
-    pub fn get_actual(&self) -> Vec<f32> {
-        return self.imu.get_actual_pos();
+    pub fn get_actual(&self) -> Vec<f64> {
+        self.imu.get_actual_pos()
     }
 
     pub fn at_goal(&self) -> bool {
-        self.goal[0] < self.belief_pos[0] && self.belief_pos[0] < self.goal[0] + 10f32
-            && self.goal[1] < self.belief_pos[1] && self.belief_pos[1] < self.goal[1] + 10f32
+        self.goal[0] < self.belief_pos[0] && self.belief_pos[0] < self.goal[0] + 10.
+            && self.goal[1] < self.belief_pos[1] && self.belief_pos[1] < self.goal[1] + 10.
     }
 
-    pub unsafe fn get_error(&self) -> MvNormalParams {
-        self.imu.get_error()
+    pub unsafe fn get_error_mean(&self) -> Vec<f64> {
+        let mu = self.imu.get_error_mean();
+        vec![mu[0], mu[1]]
+    }
+
+    pub unsafe fn get_error_covariance(&self) -> Vec<f64> {
+        let cov = self.imu.get_error_cov();
+        vec![cov[0], cov[1], cov[2], cov[3]]
     }
 }
 
 impl Kalman {
 
-    pub fn new(x: f32, y: f32) -> Kalman {
+    pub fn new(x: f64, y: f64) -> Kalman {
         Kalman {
             belief_pos: Vec::from([x, y]),
             state: Vec::from([0.0, 0.0]),
-            covariance_matrix: Box::new(arr2(&[
-                [1.0, 0.0],
-                [0.0, 1.0]
-            ])),
-            goal: Vec::from([-1f32, -1f32]),
+            covariance_matrix: Box::new(DMatrix::from_vec(2, 2, vec![0.0, 0.0, 0.0, 0.0])),
+            goal: Vec::from([-1., -1.]),
             imu: Imu::new(x, y),
         }
     }
@@ -80,10 +81,10 @@ impl Kalman {
      * Update 1 ms of movement.
      */
     pub unsafe fn tick(&mut self, universe: Universe) {
-        let mut velocity: f32 = self.state[0];
-        let mut theta: f32 = self.state[1];
+        let mut velocity = self.state[0];
+        let mut theta = self.state[1];
         if self.at_goal() {
-            velocity = 0f32;
+            velocity = 0.;
         } else {
             // Rotate to correct based on the belief, and move at a fixed speed
             theta = self.calculate_rotation_to_goal();
@@ -95,25 +96,12 @@ impl Kalman {
 
         // Execute the kalman filter
         // Step one predict the future state and state covariance matrix before measurement
-        if velocity != 0f32 {
-            let x1 = arr2(&[
-                [velocity, theta]
-            ]);
-            let z = arr2(&[
-                [self.state[0] - velocity, self.state[1] - theta]
-            ]);
-            let Q = arr2(&[
-                [0.01, 0.001],
-                [0.001, 0.01]
-            ]);
-            let R = arr2(&[
-                [0.01, 0.001],
-                [0.001, 0.01]
-            ]);
-            let I = arr2(&[
-                [1.0, 0.0],
-                [0.0, 1.0]
-            ]);
+        if velocity != 0. {
+            let x1 = DVector::from_vec(vec![velocity, theta]);
+            let z = DVector::from_vec(vec![self.state[0] - velocity, self.state[1] - theta]);
+            let Q = DMatrix::from_vec(2, 2, vec![0.01, 0.001, 0.001, 0.01]);
+            let R = DMatrix::from_vec(2, 2, vec![0.01, 0.001, 0.001, 0.01]);
+            let I = DMatrix::from_vec(2, 2, vec![1.0, 0.0, 0.0, 1.0]);
             let P1 = (*self.covariance_matrix).clone().add(Q.clone());
 
             // Take a measurement, save off the measurement covariance matrix
@@ -121,16 +109,16 @@ impl Kalman {
             let H = read.get_covariance_matrix();
 
             // Step 2, calculate the kalman gain, new adjusted state and state covariance matrix
-            let K = P1.clone() * H.t() * arr2(&[[1.0, 1.0], [1.0, 1.0]]) / (H.clone() * P1.clone() * H.t() + R.t());
+            let K = P1.clone() * H.transpose() * (DMatrix::from_vec(2, 2, vec![1.0, 1.0, 1.0, 1.0]).component_div(&(H.clone() * P1.clone() * H.transpose() + R.transpose())));
             let x = x1.clone().add(K.clone().mul(z.sub(H.clone().mul(x1.clone()))));
             let P = I.sub(K.clone().mul(H.clone())).mul(P1.clone());
-            self.state[0] = *x.index([0, 0]);
-            self.state[1] = *x.index([0, 1]);
+            self.state[0] = x[0];
+            self.state[1] = x[1];
             *self.covariance_matrix = P;
 
             // Update the beliefs of the x and y position using our new adjusted state
-            self.belief_pos[0] = (self.belief_pos[0] + self.state[1].cos() * self.state[0]).max(0f32).min(universe.width as f32);
-            self.belief_pos[1] = (self.belief_pos[1] + self.state[1].sin() * self.state[0]).max(0f32).min(universe.height as f32);
+            self.belief_pos[0] = (self.belief_pos[0] + self.state[1].cos() * self.state[0]).max(0.).min(universe.width as f64);
+            self.belief_pos[1] = (self.belief_pos[1] + self.state[1].sin() * self.state[0]).max(0.).min(universe.height as f64);
 
             log(&format!("Kalman filter complete. x: {}, y: {}, velocity: {}, rotation: {}, state_covariance: {}", self.belief_pos[0], self.belief_pos[1], self.state[0], self.state[1], self.covariance_matrix.to_string()));
         }
@@ -139,7 +127,7 @@ impl Kalman {
     /*
      * Calculate the rotation required to get to the goal.
      */
-    fn calculate_rotation_to_goal(&self) -> f32 {
+    fn calculate_rotation_to_goal(&self) -> f64 {
         let delta_x = self.goal[0] - self.belief_pos[0];
         let delta_y = self.goal[1] - self.belief_pos[1];
         let theta = (delta_y.abs() / delta_x.abs()).atan();
@@ -148,20 +136,20 @@ impl Kalman {
         if delta_x.is_sign_positive() && delta_y.is_sign_positive() {
             theta
         } else if !delta_x.is_sign_positive() && delta_y.is_sign_positive() {
-            180f32.to_radians() - theta
+            180f64.to_radians() - theta
         } else if !delta_x.is_sign_positive() && !delta_y.is_sign_positive() {
-            theta - 180f32.to_radians()
+            theta - 180f64.to_radians()
         } else if delta_x.is_sign_positive() && !delta_y.is_sign_positive() {
-            360f32.to_radians() - theta
+            360f64.to_radians() - theta
         } else {
-            0f32
+            0.
         }
     }
 
     /*
      * Sets a goal.
      */
-    pub fn set_goal(&mut self, goal_x: f32, goal_y: f32) {
+    pub fn set_goal(&mut self, goal_x: f64, goal_y: f64) {
         self.goal[0] = goal_x;
         self.goal[1] = goal_y;
     }
@@ -169,7 +157,7 @@ impl Kalman {
     /*
      * Set the rotation error, used in test.
      */
-    pub unsafe fn set_error(&mut self, m: Vec<f32>, std: Matrix<f32>) {
-        self.imu.set_error(m, std);
+    pub unsafe fn set_error(&mut self, m: Vec<f64>, covariance: Vec<f64>) {
+        self.imu.set_error(m, covariance);
     }
 }
